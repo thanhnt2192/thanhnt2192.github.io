5d519a296735164c469d8c6ebb861efd1a59b988
Thanh <thanhnt@ai-t.vn>
2023-08-21 17:34:45 +0700

    core

---
+++ js/core.js
@@ -0,0 +1,80 @@
+window.game.fps = {
+  value: 60
+};
+
+window.game.screen = {
+  width: 160,
+  height: 144,
+  scale: 4
+};
+
+window.game.button = {
+  up: {
+    press: false,
+    release: false,
+    hold: false
+  },
+  down: {
+    press: false,
+    release: false,
+    hold: false
+  },
+  left: {
+    press: false,
+    release: false,
+    hold: false
+  },
+  right: {
+    press: false,
+    release: false,
+    hold: false
+  },
+  a: {
+    press: false,
+    release: false,
+    hold: false
+  },
+  b: {
+    press: false,
+    release: false,
+    hold: false
+  }
+};
+
+window.game.keymap = {
+  "87": "up", // W
+  "83": "down", // S
+  "65": "left", // A
+  "68": "right", // D
+  "76": "a", // L
+  "80": "b" // P
+};
+
+window.game.run = function () {
+  for (const button of this.button) {
+    button.press = false;
+    button.release = false;
+  }
+};
+
+window.game.launch = function (canvas, { document }) {
+  this.fps.interval = 1000 / this.fps.value;
+  this.screen.context = canvas.getContext("2d");
+  this.screen.context.imageSmoothingEnabled = false;
+  document.addEventListener("keydown", function (event) {
+    var key = this.keymap[event.keyCode.toString()];
+    var button = this.button[key];
+    if (button) {
+      button.press = true;
+      button.hold = true;
+    }
+  }.bind(this));
+  document.addEventListener("keyup", function (event) {
+    var key = this.keymap[event.keyCode.toString()];
+    var button = this.button[key];
+    if (button) {
+      button.release = true;
+      button.hold = false;
+    }
+  }.bind(this));
+};
