fe60f342df9f884b8c39c6a1d2e81c48447d987b
Thanh <thanhnt@ai-t.vn>
2024-01-19 17:35:23 +0700

    draft

--- js/conversation.js
+++ js/conversation.js
@@ -1,26 +1,29 @@
-window.game.initializeConversation = function () {
-  this.scx = 0;
-  this.scy = 0;
-  this.balls = [
-    {
-      tilemap: [[this.tileset.ball]],
-      position: {
-        x: 0,
-        y: 0,
-        absolute: false
+window.game.data["conversation"] = {
+  "dialog": {
+    "tilemap": [],
+    "position": {
+      "x": 0,
+      "y": 0,
+      "absolute": false
+    },
+    "script": [],
+    "cursor": 0
+  },
+  "initallize": function () {
+    this.scroll["x"] = 0;
+    this.scroll["y"] = 0;
+  },
+  "render": function () {
+    const { input, tileset, data } = this;
+    const unicode = tileset["unicode"];
+    const conversation = data["conversation"];
+    const dialog = conversation["dialog"];
+    if (input["a"]["press"]) {
+      dialog["cursor"]++;
+      let line = dialog["script"][dialog["cursor"]][0];
+      for (let i = 0; i < line.length; i++) {
+        dialog["tilemap"][1][1 + i] = unicode[line[i]];
       }
-    }
-  ];
-};
-
-window.game.renderConversation = function () {
-  const { dialog } = this.data;
-  const { unicode } = this.tileset;
-  if (this.input.a.press) {
-    dialog.cursor++;
-    let line = dialog.script[dialog.cursor][0];
-    for (let i = 0; i < line.length; i++) {
-      dialog.tilemap[1][1 + i] = unicode[line[i]];
     }
   }
 };
