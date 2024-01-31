a38403ab6815a1bf422ac593b0c3b3f8ac470998
Thanh <thanhnt@ai-t.vn>
2024-01-31 14:19:52 +0700

    init

--- js/init.js
+++ js/init.js
@@ -1,9 +1,4 @@
-window.game["scroll"] = { "x": 0, "y": 0 };
-
-window.game["render"] = function ({ draw, scroll, call, bind, input }) {
-  this.scx = 0;
-  this.scy = 0;
-
+window.game["render"] = function (core) {
   const unicode = this.tileset["unicode"];
   this.logo = {
     tilemap: [[unicode["N"], unicode["o"], unicode["D"], unicode["M"], unicode["G"], unicode["."], unicode["C"], unicode["O"], unicode["M"]]],
@@ -14,7 +9,5 @@ window.game["render"] = function ({ draw, scroll, call, bind, input }) {
     }
   };
 
-  const { ["conversation"]: conversation } = this.data;
-  this.call(conversation["initialize"]);
-  this.render = this.bind(conversation["render"]);
+  core.screen.draw(this.logo);
 };
