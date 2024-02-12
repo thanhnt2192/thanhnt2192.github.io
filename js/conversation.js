2d0949ad80e848d09a93de46c106fb9bb00d6718
thanhnt21 <thanhnt2192@gmail.com>
2024-02-12 22:43:05 +0700

    dialog

--- js/conversation.js
+++ js/conversation.js
@@ -1,29 +1,45 @@
 window.game["conversation"] = {
-  "dialog": {
-    "tilemap": [[], [], [], []],
-    "position": {
-      "x": 0,
-      "y": 0,
-      "absolute": false
-    },
-    "script": [
-      ["Hello, I am Main", ""],
-      ["Hello, I am Main", ""]
-    ],
-    "cursor": 0
-  },
+  "script": [
+    ["Hello, I am Main", ""],
+    ["Hello, I am Main", ""]
+  ],
+  "cursor": 0,
   "load": function () {
     const unicode = this["tileset"]["unicode"];
-    const dialog = this.conversation["dialog"];
-    let line = dialog["script"][dialog["cursor"]][0];
+    const dialog = this["conversation"]["dialog"];
+    let line = this["conversation"]["script"][this["conversation"]["cursor"]][0];
     let i = 0;
     while (i < line["length"]) {
-      dialog["tilemap"][1][1 + i] = unicode[line[i]];
+      dialog["text"]["tilemap"][1][1 + i] = unicode[line[i]];
       i++;
     }
   },
   "initialize": function (core) {
     core.screen.scroll(0, 0);
+    const white = this["tileset"]["white"];
+    this["conversation"]["dialog"] = {
+      "background": {
+        "tilemap": [
+          [white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white],
+          [white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white],
+          [white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white],
+          [white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white]
+        ],
+        "position": {
+          "x": 0,
+          "y": 112,
+          "absolute": false
+        }
+      },
+      "text": {
+        "tilemap": [[], [], [] , []],
+        "position": {
+          "x": 0,
+          "y": 112,
+          "absolute": false
+        }
+      }
+    };
     core.call(this["conversation"]["load"], []);
   },
   "render": function (core) {
@@ -42,6 +58,7 @@ window.game["conversation"] = {
       }
     }
 
-    core.screen.draw(dialog);
+    core.screen.draw(dialog.background);
+    core.screen.draw(dialog.text);
   }
 };
