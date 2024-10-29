013ed40890517661c2d920f1cb32cf99fb6ad92c
Thanh <thanhnt@ai-t.vn>
2024-10-29 17:57:20 +0700

    animation

--- js/road.js
+++ js/road.js
@@ -22,8 +22,46 @@ window.app["road"] = {
         "y": 100,
         "absolute": false
       },
+      "animation": {
+        "walk": {
+          "a": 1,
+          "index": 1,
+          "delay": 0,
+          "animate": function (core) {
+            const animation = this["road"]["player"]["animation"]["walk"];
+            animation["delay"] += core.timestep;
+            if (animation["delay"] > 200) {
+              animation["delay"] -= 200;
+            } else {
+              return;
+            }
+            if (animation["index"] !== 1) {
+              animation["a"] = 0 - animation["a"];
+            }
+            animation["index"] += animation["a"];
+            this["road"]["player"]["tilemap"] = this["tileset"]["player"][animation["index"]];
+          }
+        }
+      },
+      "interval": {
+        "move": 0
+      },
       "move": function (core) {
-        //
+        const player = this["road"]["player"];
+        if (core.control.left.hold || core.control.right.hold) {
+          player["interval"]["move"] += core.timestep;
+          const interval = 20;
+          while (player["interval"]["move"] > interval) {
+            player["interval"]["move"] -= interval;
+            if (core.control.left.hold) {
+              player["position"]["x"] -= 1;
+            }
+            if (core.control.right.hold) {
+              player["position"]["x"] += 1;
+            }
+          }
+          core.call(player["animation"]["walk"]["animate"], [core]);
+        }
       }
     };
     this["road"]["qi"] = {
@@ -36,6 +74,7 @@ window.app["road"] = {
     };
   },
   "render": function (core) {
+    core.call(this["road"]["player"]["move"], [core]);
     core.screen.draw(this["road"]["background"]);
     core.screen.draw(this["road"]["player"]);
     core.screen.draw(this["road"]["qi"]);
