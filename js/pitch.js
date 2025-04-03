3a4c39afe39811f96e9b587358bebbecc8d7a9a5
thanhnt21 <thanhnt2192@gmail.com>
2025-04-04 00:12:57 +0700

    draw

--- js/pitch.js
+++ js/pitch.js
@@ -18,14 +18,32 @@ window.app["pitch"] = {
     this["pitch"]["player"] = {
       "tilemap": this["tileset"]["player"]["ready"][0],
       "position": {
-        "x": 50,
-        "y": 100,
+        "x": 88,
+        "y": 110,
+        "absolute": false
+      }
+    };
+    this["pitch"]["shadow"] = {
+      "tilemap": [[this["tileset"]["shadow"]]],
+      "position": {
+        "x": 84,
+        "y": 120,
+        "absolute": false
+      }
+    };
+    this["pitch"]["ball"] = {
+      "tilemap": [[this["tileset"]["ball"][0]]],
+      "position": {
+        "x": 84,
+        "y": 118,
         "absolute": false
       }
     };
   },
   "render": function (core) {
     core.screen.draw(this["pitch"]["background"]);
+    core.screen.draw(this["pitch"]["shadow"]);
+    core.screen.draw(this["pitch"]["ball"]);
     core.screen.draw(this["pitch"]["player"]);
   }
 };
