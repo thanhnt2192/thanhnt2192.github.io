81d6a1e4f8b734cd80e07d1abe086f04c8cd6eec
Thanh <thanhnt@ai-t.vn>
2025-07-21 17:22:48 +0700

    font

--- arena/core.js
+++ arena/core.js
@@ -1,10 +1,10 @@
-// const touchHandler = (ev) => {
-// 	ev.preventDefault() // Prevent text selection
-// }
-// document.addEventListener('touchstart', touchHandler, {passive:false})
-// document.addEventListener('touchmove', touchHandler, {passive:false})
-// document.addEventListener('touchend', touchHandler, {passive:false})
-// document.addEventListener('touchcancel', touchHandler, {passive:false})
+const touchHandler = (ev) => {
+	ev.preventDefault() // Prevent text selection
+}
+document.addEventListener('touchstart', touchHandler, {passive:false})
+document.addEventListener('touchmove', touchHandler, {passive:false})
+document.addEventListener('touchend', touchHandler, {passive:false})
+document.addEventListener('touchcancel', touchHandler, {passive:false})
 
 document.getElementById("btn-end-turn").onclick = function () {
   document.getElementsByClassName("player-field")[0].classList.add("closed");
