d21a7930f283242da48957f12fb3c25cc64aa6ef
Thanh <thanhnt@ai-t.vn>
2026-06-30 17:09:30 +0700

    function createGroup

--- svg/js/app.js
+++ svg/js/app.js
@@ -1,9 +1,13 @@
-createPath({
-  "fill": "red",
-  "d": "M10,20 H80 V30 H10 Z"
-});
+const app = document.getElementById("app");
 
-createPath({
+app.appendChild(createGroup({}, [
+  {
+    "fill": "red",
+    "d": "M10,20 H80 V30 H10 Z"
+  }
+]));
+
+app.appendChild(createPath({
   "fill": "#000000",
   "d": "M1,16 h10 v-2 h-1 v1 h-8 v-1 h-1 Z m1,-2 h8 v-3 h-1 v2 h-6 v-1 h-1 z m1,-2 h1 v-1 h-1 z m1,-1 h1 v-1 h-1 z m1,-1 h1 v-2 h-3 v1 h2 z m-4,0 h2 v-1 h-2 z m0,-1 v-2 h-1 v2 z m0,-2 h1 v-1 h-1 z m1,-1 h1 v-1 h-1 z m1,-1 h1 v-1 h-1 z m0,-1 v-1 h3 v-1 h-4 v2 z m3,0 h3 v-1 h-3 z m3,0 h1 v1 h-1 z m1,1 v6 h1 v-6 z m-3,2 v1 h-1 v-1 z"
-});
+}));
--- svg/js/core.js
+++ svg/js/core.js
@@ -1,10 +1,22 @@
-const app = document.getElementById("app");
-
 function createPath(attributes) {
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
   for (let key in attributes) {
     path.setAttribute(key, attributes[key]);
   }
-  app.appendChild(path);
   return path;
+}
+
+function createGroup(attributes, paths) {
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  for (let key in attributes) {
+    group.setAttribute(key, attributes[key]);
+  }
+
+  if (Array.isArray(paths)) {
+    for (const path of paths) {
+      group.appendChild(createPath(path));
+    }
+  }
+
+  return group;
 }
